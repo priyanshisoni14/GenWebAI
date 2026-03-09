6ef3aab6233f5086ea18e37a73e43fd0edb0881e
Priyanshi Soni <sonipriyanshi.4105@gmail.com>
2026-03-09 13:41:36 +0530

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+               console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log("db error")
+    }
+}
+export default connectDb;
